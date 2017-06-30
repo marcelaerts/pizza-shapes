5e43d9aa3e3e6c6fd60772c0d342dee2f334ed89
Marcel Aerts <marcel.aerts@webcolorsgroup.com>
2017-06-30 10:11:11 +0200

    Plugin fix.

--- pizza-shapes.php
+++ pizza-shapes.php
@@ -1,11 +1,14 @@
 <?php
 /**
- * Plugin Name: Pizza Shapes
- * Plugin URI: http://www.youaredigital.nl
- * Description: Plugin for testing Bedrock with a custom plugin on a personal repo.
- * Version: 1.0.1
- * Author: MrPizzaShapes
- * Author URI: http://www.youaredigital.nl
- */
+* Plugin Name: Pizza Shapes
+* Plugin URI: http://www.youaredigital.nl
+* Description: Plugin for testing Bedrock with a custom plugin on a personal repo.
+* Version: 1.0.1
+* Author: MrPizzaShapes
+* Author URI: http://www.youaredigital.nl
+*/
 
-echo "MrPizzaShapes will rise again.";
+add_action( 'wp_head', 'pizza_shapes_head' );
+function pizza_shapes_head() {
+    echo "MrPizzaShapes will rise again.";
+}
