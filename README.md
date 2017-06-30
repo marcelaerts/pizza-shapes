57bbec9c82f1271f1319c821886cbc91b28c0f87
Marcel Aerts <marcel.aerts@webcolorsgroup.com>
2017-06-30 09:35:05 +0200

    Custom Wordpress plugin.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pizza-shapes
+
+Plugin for testing Bedrock with a custom plugin on a personal repo.
+- Version: 1.0.0
---
+++ pizza-shapes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Pizza Shapes
+ * Plugin URI: http://www.youaredigital.nl
+ * Description: Plugin for testing Bedrock with a custom plugin on a personal repo.
+ * Version: 1.0.0
+ * Author: MrPizzaShapes
+ * Author URI: http://www.youaredigital.nl
+ */
+
+echo "MrPizzaShapes will rise again."
